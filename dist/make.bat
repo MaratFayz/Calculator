d5d04c1125b34f57337d0296ff20760ccaf6a5f5
Marat Fayzelgayanov <f4815162342@gmail.com>
2020-08-30 21:23:04 +0300

    Make enable to test project during packaging by maven

--- dist/make.bat
+++ dist/make.bat
@@ -2,7 +2,7 @@
 cd ..
 echo "Start building project..."
 echo "Build jar file"
-call mvn clean package -DskipTests
+call mvn clean package
 echo "Building jar files finished"
 
 echo "Build services"
