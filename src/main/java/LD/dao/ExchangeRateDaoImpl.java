ad40e2b479c74300be789a8b02fde3c80f98d152
Marat Fayzelgayanov <f4815162342@gmail.com>
2020-09-22 23:20:08 +0300

    Refactor queries for currency exchange rates

--- src/main/java/LD/dao/ExchangeRateDaoImpl.java
+++ src/main/java/LD/dao/ExchangeRateDaoImpl.java
@@ -13,6 +13,7 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import javax.persistence.metamodel.SingularAttribute;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -26,30 +27,21 @@ public class ExchangeRateDaoImpl implements ExchangeRateDao {
 
     @Override
     public BigDecimal getRateAtDate(LocalDate date, Scenario scenario, Currency currency) {
-        cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<BigDecimal> criteriaQuery = cb.createQuery(BigDecimal.class);
-
-        root = criteriaQuery.from(ExchangeRate.class);
-        criteriaQuery.select(root.get(ExchangeRate_.rate_at_date))
-                .where(
-                        cb.and(
-                                cb.equal(root.get(ExchangeRate_.exchangeRateID).get(ExchangeRateID_.scenario), scenario),
-                                cb.equal(root.get(ExchangeRate_.exchangeRateID).get(ExchangeRateID_.date), date),
-                                cb.equal(root.get(ExchangeRate_.exchangeRateID).get(ExchangeRateID_.currency), currency)
-                        )
-                );
-
-        TypedQuery<BigDecimal> query = entityManager.createQuery(criteriaQuery);
-        return query.getSingleResult();
+        return getRateByDateScenarioCurrencyType(date, scenario, currency, ExchangeRate_.rate_at_date);
     }
 
     @Override
     public BigDecimal getAverageRateAtDate(LocalDate date, Scenario scenario, Currency currency) {
+        return getRateByDateScenarioCurrencyType(date, scenario, currency, ExchangeRate_.average_rate_for_month);
+    }
+
+    private BigDecimal getRateByDateScenarioCurrencyType(LocalDate date, Scenario scenario, Currency currency,
+                                                         SingularAttribute<ExchangeRate, BigDecimal> rateType) {
         cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<BigDecimal> criteriaQuery = cb.createQuery(BigDecimal.class);
 
         root = criteriaQuery.from(ExchangeRate.class);
-        criteriaQuery.select(root.get(ExchangeRate_.average_rate_for_month))
+        criteriaQuery.select(root.get(rateType))
                 .where(
                         cb.and(
                                 cb.equal(root.get(ExchangeRate_.exchangeRateID).get(ExchangeRateID_.scenario), scenario),
