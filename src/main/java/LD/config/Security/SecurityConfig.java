ed4e0ccc4b03c9b132c19949d2a96e94ae14e424
Marat Fayzelgayanov <f4815162342@gmail.com>
2020-10-21 20:31:57 +0300

    Fix IntegrationTest

--- src/main/java/LD/config/Security/SecurityConfig.java
+++ src/main/java/LD/config/Security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().and().cors().disable()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login").not().authenticated()
                 .antMatchers("/admin").hasAnyAuthority(
--- src/test/java/LD/IntegrationTest.java
+++ src/test/java/LD/IntegrationTest.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -49,6 +50,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
 @Testcontainers
 @Log4j2
+@TestPropertySource(properties = {"enable.security.in.project=true"})
 public class IntegrationTest {
 
     private MockMvc mockMvc;
